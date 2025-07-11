b830f3f643a87da2af19692e7f83f1627ca56287
Nitesh kumar <nk41521391@gmail.com>
2025-07-11 22:22:47 +0530

    new file added

---
+++ appC.js
@@ -0,0 +1,5 @@
+const mathOperations = require("./mathOperationC.js");
+
+console.log(mathOperations.add(2, 2));
+console.log(mathOperations.multiply(3, 4));
+console.log(mathOperations.sub(5, 2));
---
+++ appM.js
@@ -0,0 +1,9 @@
+//default import
+import multiply from "./mathOperationM.js";
+
+//name import
+import { add, sub } from "./mathOperationM.js";
+
+console.log(multiply(2, 2));
+console.log(add(-1, 7));
+console.log(sub(9, 5));
